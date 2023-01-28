9161d90d6ba5e51597bdcc172d183ae894fe38fd
Nishant <nishantdhamija41@gmail.com>
2023-01-29 02:54:57 +0530

    added mongodb

--- GenerateAJWT.go
+++ GenerateAJWT.go
@@ -1,30 +1,67 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
-
 
 var sampleSecretKey = []byte("uGwk9h/Sj29wYNoLKHF54elBgbim8Z87+51j0wklbkyy+dkLZhDiu7N+8K71Zquv")
 
+type User struct {
+	Id       primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Email    string             `json:"email"`
+	UserName string             `json:"username"`
+	Password string             `json:"password"`
+}
 
-func generateJWT(c *gin.Context){
-token := jwt.New(jwt.SigningMethodHS256)
-//c.JSON(http.StatusInternalServerError,gin.H{"message":token})
-claims:=token.Claims.(jwt.MapClaims)
-claims["exp"]=time.Now().Add(10*time.Minute)
-claims["authorized"] = true
-tokenString, err := token.SignedString(sampleSecretKey)
-if err != nil {
-	fmt.Print("ello->",err);
-     c.JSON(http.StatusInternalServerError,gin.H{"message":err})
-	 return
- }
+func generateJWT(c *gin.Context) {
+	token := jwt.New(jwt.SigningMethodHS256)
+	claims := token.Claims.(jwt.MapClaims)
+	claims["exp"] = time.Now().Add(10 * time.Minute)
+	claims["authorized"] = true
+	tokenString, err := token.SignedString(sampleSecretKey)
+	if err != nil {
+		fmt.Print("ello->", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"message": err})
+		return
+	}
+	c.Writer.Header().Add("Access-Control-Allow-Origin", "*")
+	c.Writer.Header().Add("Access-Control-Allow-Credentials", "true")
+	c.Writer.Header().Add("Access-Control-Max-Age", "1800")
+	c.Writer.Header().Add("Access-Control-Allow-Headers", "content-type")
+	c.Writer.Header().Add("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS")
+	c.JSON(http.StatusOK, gin.H{"token": tokenString})
+}
 
-c.JSON(http.StatusOK,gin.H{"token":tokenString})
+func AuthMiddleWare() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		auth := c.GetHeader("auth")
+		fmt.Print("camehre", auth)
+		if auth == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"message": "No jwt token in header"})
+			return
+		}
+		token, _ := jwt.Parse(auth, func(token *jwt.Token) (interface{}, error) {
+			_, ok := token.Method.(*jwt.SigningMethodHMAC)
+			if !ok {
+				return nil, errors.New("DDu")
+			}
+			return "", errors.New("DDu")
+		})
+
+		if token == nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"message": "No jwt token in header"})
+			return
+		}
+	}
+}
+
+func loginAUser(c *gin.Context) {
+
 }
--- main.go
+++ main.go
@@ -1,18 +1,48 @@
 package main
 
 import (
+	"context"
 	"fmt"
-    "github.com/gin-gonic/gin"
-	
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+var collection *mongo.Collection
+
+const db = "userDB"
+const colName = "users"
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+	uri:=os.Getenv("MONGODB")
+	fmt.Print("NISNAT CAME ,", uri)
+	clientOptions := options.Client().ApplyURI(os.Getenv("MONGODB"))
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal("Nishant", err, os.Getenv("MONGODB"))
+	}
+
+	collection = (*mongo.Collection)(client.Database(db).Collection(colName))
+	title:="null"
+	collection.InsertOne(context.TODO(),title)
+}
 func main() {
-	fmt.Println("Hello world");
+	fmt.Println("Hello world")
 	handleFunc()
 }
-func handleFunc(){
-	r:=gin.Default()
-	r.GET("jwt",generateJWT)
-	r.GET("/signup",handlePage)
+func handleFunc() {
+	r := gin.Default()
+	public := r.Group("/create")
+	public.POST("/join", generateJWT)
+	public.POST("/login", loginAUser)
+	r.GET("/signup", handlePage)
+	r.Use(AuthMiddleWare())
 	r.Run()
 }
